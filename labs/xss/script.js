77d985be4d6a9c8c3487804a135631dd72c600b2
Krishna Dwivedi <150330334+SudoHopeX@users.noreply.github.com>
2025-08-04 19:14:00 +0530

    Update script.js

--- labs/xss/script.js
+++ labs/xss/script.js
@@ -1,6 +1,6 @@
 function checkOrientation() {
   const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  const isPortrait = window.innerWidth < 490 && window.innerHeight > window.innerWidth;
+  const isPortrait = window.innerWidth < 1090 && window.innerHeight > window.innerWidth;
   const existing = document.getElementById('orientation-message');
 
   if (isMobile && isPortrait) {
