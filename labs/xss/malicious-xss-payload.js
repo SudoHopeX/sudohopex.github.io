bf6244dcbfef5ed803f972f7c74867f40257663b
Krishna Dwivedi <150330334+SudoHopeX@users.noreply.github.com>
2025-09-03 13:35:49 +0530

    Update and rename malicious-xss-payload.css to malicious-xss-payload.js

---
+++ labs/xss/malicious-xss-payload.js
@@ -0,0 +1,7 @@
+// body {
+//     background: pink !important;
+//     @import url("data:text/html,<script>alert(window.origin)</script>");
+// /*     background-image: url('data:text\javascript,alert(window.origin)'); */
+// }
+
+alert(window.origin);
